2752a48b3a4a55e3283c9bb65b920e883ad472c0
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-10-29 10:07:42 +0100

    Create neon_db.js

---
+++ neon_db.js
@@ -0,0 +1,39 @@
+
+
+const NEON_ACCOUNTS = [
+  
+    {
+        id: 1,
+        active_db_limit: 3, // Target limit for this account
+        api_key: 'napi_u8ax0jt0avsf3jv9lpucn6osv7d4s1x8gtb7c8dp6yup6xvnlfu2r4236cb2fnyk',
+        project_id: 'late-sky-11531843',
+        branch_id: 'br-plain-sea-ah8rvcfe',
+        db_host: 'ep-spring-haze-ahk1axyx-pooler.c-3.us-east-1.aws.neon.tech',
+        db_user: 'neondb_owner',
+        db_password: 'npg_n0Q8PeICthOw'
+    },
+    
+    {
+        id: 2,
+        active_db_limit: 3,
+        api_key: 'YOUR_NEON_API_KEY_2',
+        project_id: 'late-sky-11531843',
+        branch_id: 'br-plain-sea-ah8rvcfe',
+        db_host: 'ep-example-host-2.aws.neon.tech',
+        db_user: 'neondb_owner_2',
+        db_password: 'your_password_2'
+    },
+  
+    {
+        id: 34,
+        active_db_limit: 3,
+        api_key: 'YOUR_NEON_API_KEY_34',
+        project_id: 'project_id_34',
+        branch_id: 'branch_id_34',
+        db_host: 'ep-example-host-34.aws.neon.tech',
+        db_user: 'neondb_owner_34',
+        db_password: 'your_password_34'
+    }
+];
+
+module.exports = { NEON_ACCOUNTS };
