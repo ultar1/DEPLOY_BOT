c1111bebd1a252245dcf7f1598e11899dec1bb54
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-11-03 21:58:58 +0100

    Update neon_db.js

--- neon_db.js
+++ neon_db.js
@@ -3,27 +3,30 @@
 const NEON_ACCOUNTS = [
   
     {
-        id: 1,
-        active_db_limit: 2, // Target limit for this account
-        api_key: 'napi_u8ax0jt0avsf3jv9lpucn6osv7d4s1x8gtb7c8dp6yup6xvnlfu2r4236cb2fnyk',
-        project_id: 'late-sky-11531843',
-        branch_id: 'br-plain-sea-ah8rvcfe',
-        db_host: 'ep-spring-haze-ahk1axyx-pooler.c-3.us-east-1.aws.neon.tech',
-        db_user: 'neondb_owner',
-        db_password: 'npg_n0Q8PeICthOw'
-    },
+        
+    id: 1,
+    active_db_limit: 2,
+    api_key: 'napi_z06jk6wo6esdke2368yqug2zvy833vlkg4k0sig228c6tpyqlprg9ujbswark0qv',
+    project_id: 'solitary-butterfly-01100200',
+    branch_id: 'br-holy-rice-a4xft487',
+    db_host: 'ep-cool-fog-a4fk9d4o-pooler.us-east-1.aws.neon.tech',
+    db_user: 'neondb_owner',
+    db_password: 'npg_g5kIr3UdxhSs'
+},
+
     
       
     {
-        id: 2,
-        active_db_limit: 2,
-        api_key: 'napi_fl3f6mbrtpmfu5r54z072silsu1y6cnfsm6mjj9x6r71itinuvwaf3qa6f04wg4d', 
-        project_id: 'weathered-bush-38000036', 
-        branch_id: 'br-red-flower-ahd3oxcy', 
-        db_host: 'ep-hidden-cloud-aht0m05k-pooler.c-3.us-east-1.aws.neon.tech', 
-        db_user: 'neondb_owner', 
-        db_password: 'npg_KCp7QdTo8sUY'
-    },
+    id: 2,
+    active_db_limit: 2,
+    api_key: 'napi_yqjf0u3qmd3awm4oq67hcr8olzi2zprfru1an3a92gmcy6gv2e4382utvxfv6p1e',
+    project_id: 'calm-sound-36464652',
+    branch_id: 'br-withered-cloud-a44wsqzj',
+    db_host: 'ep-spring-water-a421mch5-pooler.us-east-1.aws.neon.tech',
+    db_user: 'neondb_owner',
+    db_password: 'npg_vsIaXRcVH8h1'
+},
+
   
         
     {
