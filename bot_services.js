f8e56ba2f8ffe7f412fe580b52ec0b1ccf70f716
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-11-20 01:57:09 +0100

    Update bot_services.js

--- bot_services.js
+++ bot_services.js
@@ -2722,7 +2722,6 @@ module.exports = {
     getExpiredBackups,
     getBlacklistedNames,
     removeBlacklistedName,
-    addBlacklistedName,
     setGroupWelcomeMessage,
     setGroupWelcome,
     getGroupSettings,
